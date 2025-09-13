5fec5b253ec16775d7bde5dcc2b9ff9b35fb4369
andersonhavah <havahanderson@gmail.com>
2025-09-13 00:24:17 +0000

    Added the index route

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ app.set("layout", "./layouts/layout");
  *************************/
 app.use(static)
 
+// Index route
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" });
+});
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
