ea99950c66b183e204e4621b244361a4fd7421bd
andersonhavah <havahanderson@gmail.com>
2025-10-01 16:36:43 +0000

    Added Express Error Handler and File Not Found Route.

--- server.js
+++ server.js
@@ -5,13 +5,14 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
+const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require("./utilities/");
 
 /* ***********************
  * View Engine and Templates
@@ -30,10 +31,34 @@ app.use(static)
 // app.get("/", function (req, res) {
 //   res.render("index", { title: "Home" });
 // });
-app.get("/", baseController.buildHome);
+app.get("/", utilities.handleErrors(baseController.buildHome));
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) {
+    message = err.message;
+  } else {
+    message = 'Seems like you are out of space. The page is gone. Please come back to us. 🤗';
+  }
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
