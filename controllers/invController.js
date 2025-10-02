8a03e5b31a97a9cd282a1ca58adc2aac8d3d3a91
andersonhavah <havahanderson@gmail.com>
2025-10-02 18:36:55 +0000

    Added the "Build vehicle detail view" code to the inventory controller.

--- controllers/invController.js
+++ controllers/invController.js
@@ -16,7 +16,34 @@ invCont.buildByClassificationId = async function (req, res, next) {
         title: className + " vehicles",
         nav,
         grid,
+        description: `This is the ${className} vehicles page.`,
     })
 }
+
+/* ***************************
+ * Build vehicle detail view
+ * ************************** */
+invCont.buildByInventoryId = async function (req, res, next) {
+    const inventory_id = req.params.inventoryId;
+    const data = await invModel.getVehicleByInventoryId(inventory_id);
+    
+    // Check if data was returned
+    if (!data) {
+      // If no data, create an error and pass to the error handler
+      const err = new Error("Vehicle not found.");
+      err.status = 404;
+      return next(err);
+    }
+    
+    const grid = await utilities.buildVehicleDetailGrid(data);
+    let nav = await utilities.getNav();
+    const vehicleName = `${data.inv_year} ${data.inv_make} ${data.inv_model}`;
+    res.render("./inventory/detail", {
+      title: vehicleName,
+      nav,
+      grid, // Use "grid" to pass the HTML to the view
+      description: `This is the ${vehicleName} page.`,
+    });
+  };
 
 module.exports = invCont;
