2e862e956cbe167795b45f41b7368852fc4dd53b
andersonhavah <havahanderson@gmail.com>
2025-10-09 14:29:00 +0000

    Created and Built the account route.

---
+++ routes/accountRoute.js
@@ -0,0 +1,13 @@
+// Needed Resources
+const express = require("express");
+const router = new express.Router();
+const utilities = require("../utilities/");
+const accountController = require("../controllers/accountController");
+
+// Deliver Login View
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+
+// Deliver Registration View
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
+module.exports = router;
