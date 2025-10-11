420001c40c346ae46d2f2ba4cf531d30e0fab5d5
andersonhavah <havahanderson@gmail.com>
2025-10-11 22:47:57 +0000

    Added post routes for processing registration and login.

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,11 +3,29 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities/");
 const accountController = require("../controllers/accountController");
+const regValidate = require("../utilities/account-validation");
 
 // Deliver Login View
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
 // Deliver Registration View
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
+// Process Registration
+router.post(
+    "/register",
+    regValidate.registrationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount));
+
+// Process the login attempt
+router.post(
+    "/login",
+    regValidate.loginRules(),
+    regValidate.validateLoginData,
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
 
 module.exports = router;
