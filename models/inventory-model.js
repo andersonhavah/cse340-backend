76e4ea4381028edc9f976c54dbfea85a446da562
andersonhavah <havahanderson@gmail.com>
2025-10-15 23:52:04 +0000

    Added functions

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -37,6 +37,69 @@ async function getVehicleByInventoryId(inventory_id) {
       console.error("getvehiclebyinventoryid error " + error)
       return null // Return null if an error occurs or no vehicle is found
     }
-  }
+}
+  
+/* ****************************************
+ * Add a new classification to the database
+ * Task 2
+ * *************************************** */
+async function addClassification(classification_name) {
+    try {
+        const sql = "INSERT INTO classification (classification_name) VALUES ($1) RETURNING *";
+        const result = await pool.query(sql, [classification_name]);
+        return result.rows[0];
+    } catch (error) {
+        console.error("addClassification error: " + error);
+        return null;
+    }
+}
 
-module.exports = { getClassifications, getInventoryByClassificationId, getVehicleByInventoryId };
+
+/* ****************************************
+ * Add a new inventory item to the database
+ * Task 3
+ * *************************************** */
+async function addInventory(
+    inv_make,
+    inv_model,
+    inv_year,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_miles,
+    inv_color,
+    classification_id
+) {
+    try {
+        const sql = `INSERT INTO public.inventory (
+            inv_make,
+            inv_model,
+            inv_year,
+            inv_description,
+            inv_image,
+            inv_thumbnail,
+            inv_price,
+            inv_miles,
+            inv_color,
+            classification_id
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`;
+        return await pool.query(sql, [
+            inv_make,
+            inv_model,
+            inv_year,
+            inv_description,
+            inv_image,
+            inv_thumbnail,
+            inv_price,
+            inv_miles,
+            inv_color,
+            classification_id
+        ]);
+    } catch (error) {
+        console.error("addInventory error: " + error);
+        return null;
+    }
+}
+
+module.exports = { getClassifications, getInventoryByClassificationId, getVehicleByInventoryId, addClassification, addInventory };
