2b6a022841c357cedefc6c8e9e2ab92bf0a81812
andersonhavah <havahanderson@gmail.com>
2025-10-17 02:09:18 +0000

    Added checkUpdateData function

--- utilities/inventory-validation.js
+++ utilities/inventory-validation.js
@@ -118,4 +118,49 @@ validate.checkInventoryData = async (req, res, next) => {
     next();
 };
 
+/* ******************************
+ * Check inventory data for errors during UPDATE
+ * ***************************** */
+validate.checkUpdateData = async (req, res, next) => {
+    const {
+        inv_id, // Add inv_id
+        inv_make,
+        inv_model,
+        inv_year,
+        inv_description,
+        inv_image,
+        inv_thumbnail,
+        inv_price,
+        inv_miles,
+        inv_color,
+        classification_id
+    } = req.body;
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        let nav = await utilities.getNav();
+        const classificationSelect = await utilities.buildClassificationList(classification_id);
+        const itemName = `${inv_make} ${inv_model}`;
+        // Render the EDIT view on failure
+        res.render("inventory/edit-inventory", {
+            errors,
+            title: "Edit " + itemName,
+            nav,
+            classificationSelect,
+            inv_id, // Pass inv_id back
+            inv_make,
+            inv_model,
+            inv_year,
+            inv_description,
+            inv_image,
+            inv_thumbnail,
+            inv_price,
+            inv_miles,
+            inv_color,
+            classification_id
+        });
+        return;
+    }
+    next();
+};
+
 module.exports = validate;
